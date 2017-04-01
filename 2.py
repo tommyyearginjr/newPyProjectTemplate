35968af69f9fdb04fd2e55c4bf6993b1a57e8140
Tommy H. Yeargin, Jr <tommy.yeargin@gmail.com>
2017-04-01 12:04:21 -0400

    commit

--- 2.py
+++ 2.py
@@ -6,15 +6,14 @@ dirName = input("What would you like to name your directory? ")
 
 pathName = input("Enter path for your new directory (do not enter new directory name): ")
 
-newDirectoryPath = "'mkdir {}{}'".format(pathName, dirName)
-
 testString = []
 
 for i in pathName:
     testString.append(i)
 
-if testString[-1]=='/':
-    pass
+if testString[-1] == '/':
+    newDirectoryPath = "'mkdir {}{}'".format(pathName, dirName)
+
 else:
     newDirectoryPath = "'mkdir {}/{}'".format(pathName, dirName)
     
